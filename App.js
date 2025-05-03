df69f2c90a251688c52280424695a37ab7bc806e
Tamara Cvjetkovic <c.tamara02@gmail.com>
2025-05-03 10:31:01 +0200

    [WIP] AudioVisualizer

--- App.js
+++ App.js
@@ -1,89 +1,27 @@
 import React, { useState } from "react";
-import { StatusBar } from "expo-status-bar";
-import {
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
-  Button,
-  Platform,
-  NativeModules,
-  Alert,
-} from "react-native";
+import { NavigationContainer, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { StyleSheet, NativeModules, useColorScheme, View } from "react-native";
+import { SafeAreaView } from 'react-native-safe-area-context';
+import AudioVisualizer from "./components/AudioVisualizer";
+import MusicView from "./components/MusicView";
 
 const { CustomVibration } = NativeModules;
 
 export default function App() {
-  const [duration, setDuration] = useState("500"); // in ms
-  const [intensity, setIntensity] = useState("128"); // 0–255
-
-  const handleVibrate = () => {
-    if (Platform.OS === "android") {
-      const time = parseInt(duration, 10);
-      const amp = parseInt(intensity, 10);
-
-      if (isNaN(time) || isNaN(amp) || amp < 1 || amp > 255) {
-        Alert.alert(
-          "Invalid input",
-          "Please enter valid time (ms) and intensity (1–255)"
-        );
-        return;
-      }
-
-      CustomVibration.vibrateWithIntensity(time, amp);
-    } else {
-      Alert.alert(
-        "Not supported",
-        "Custom vibration is only available on Android"
-      );
-    }
-  };
+  const colorScheme = useColorScheme();
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Vibration Tester</Text>
-
-      <TextInput
-        style={styles.input}
-        keyboardType="numeric"
-        value={duration}
-        onChangeText={setDuration}
-        placeholder="Duration (ms)"
-      />
-
-      <TextInput
-        style={styles.input}
-        keyboardType="numeric"
-        value={intensity}
-        onChangeText={setIntensity}
-        placeholder="Intensity (1–255)"
-      />
-
-      <Button title="Vibrate" onPress={handleVibrate} />
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView edges={['top', 'left', 'right', 'bottom']} style = {[styles.container]} >
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>   
+        <MusicView/>
+      </ThemeProvider>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-    paddingHorizontal: 20,
-  },
-  title: {
-    fontSize: 22,
-    fontWeight: "bold",
-    marginBottom: 20,
-  },
-  input: {
-    width: "100%",
-    borderColor: "#ccc",
-    borderWidth: 1,
-    borderRadius: 8,
-    padding: 10,
-    marginBottom: 15,
-  },
+    backgroundColor: 'pink',
+ },
 });
---
+++ components/MusicView.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import AudioVisualizer from './AudioVisualizer'
+
+const MusicView = () => {
+   return (
+      <AudioVisualizer/>
+   )
+}
+
+export default MusicView
